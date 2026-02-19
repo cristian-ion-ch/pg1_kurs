57d8f203fac515b8b28ffa4a66002aa0476005c0
Cristian Ion <cristian.ion@students.ffhs.ch>
2026-02-19 16:53:35 +0100

    Doppel Übung

---
+++ src/Doppeln.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Doppeln {
+    static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+        int zahl;
+        System.out.println("Geben Sie eine Zahl ein!");
+        zahl = eingabe.nextInt();
+        zahl = 5;
+        zahl = zahl * 2;
+        System.out.println(zahl);
+    }
+}
