afb614d31022bf272473c48b9289ff542ba6294f
Cristian Ion <cristian.ion@students.ffhs.ch>
2026-02-19 17:03:03 +0100

    Kreis

---
+++ src/Kreis.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Kreis {
+    static void main(String[] args) {
+        int x;
+        Scanner eingabe = new Scanner(System.in);
+        x = eingabe.nextInt();
+        final double pi;
+        pi = 3.14159265359;
+        double flaeche;
+        flaeche = x * pi;
+        flaeche = Math.round(flaeche*100)/100.0;
+        System.out.println("Fläche" + flaeche);
+    }
+}
